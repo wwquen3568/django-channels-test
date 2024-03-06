a99f51e97422a9bf4a314c0808f51507128a05f4
wwq <wwquen3568@naver.com>
2024-03-07 00:52:20 +0900

    init: init repo

---
+++ mychatapp/mychatapp/asgi.py
@@ -0,0 +1,22 @@
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+from chat.routing import websocket_urlpatterns
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychatapp.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
